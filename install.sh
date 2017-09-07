5a844a10fb36bbdf2c214ba6688d85a8da976722
Rohan Mahajan <rmahajan@Rohan-Mahajan-MacBook-Pro.local>
2017-09-07 14:26:31 -0700

    more stuff

--- install.sh
+++ install.sh
@@ -1,14 +1,19 @@
 #!/bin/bash
 
-VUNDLE_DIR=~/.vim/bundle/Vundle.vim
+YOU_COMPLETE_ME_DIR=~/.vim/bundle/YouCompleteMe
+if [ ! -d $YOU_COMPLETE_ME_DIR ]; then
+  echo "entering the first if statement"
+  YOU_COMPLETE_MISSING=true
+fi
 if [ ! -d $VUNDLE_DIR ]; then
   git clone https://github.com/VundleVim/Vundle.vim.git $VUNDLE_DIR
 fi
 python install.py
 vim +PluginInstall +qall
 
-YOU_COMPLETE_ME_DIR=~/.vim/bundle/YouCompleteMe
-if [ ! -d $YOUCOMPLETEME_DIR ]; then
+pip install pylint
+
+if [ "$YOU_COMPLETE_MISSING" = true ]; then
   cd $YOU_COMPLETE_ME_DIR
   brew install cmake
   git submodule update --init --recursive
